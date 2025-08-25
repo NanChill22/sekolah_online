71f91be1d473de6ea051461e0336523bca7550b5
zaee07 <ihzasofyansah@gmail.com>
2025-08-25 23:11:12 +0700

    fix: perbaiki validasi form Register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,9 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        ],
+        ['password.min' => 'Password minimal harus 8 karakter.',],
+    );
 
         $user = User::create([
             'name' => $request->name,
--- resources/views/auth/auth.blade.php
+++ resources/views/auth/auth.blade.php
@@ -227,7 +227,7 @@
                 <span>or use your email for registration</span>
                 <input type="text" name="name" placeholder="Name" required />
                 <input type="email" name="email" placeholder="Email" required />
-                <input type="password" name="password" placeholder="Password" required />
+                <input type="password" name="password" placeholder="Password Min 8 karakter" required />
                 <input type="password" name="password_confirmation" placeholder="Confirm Password" required />
                 <button type="submit">Sign Up</button>
             </form>
