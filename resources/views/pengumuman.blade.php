907aaf5c0647ac42237b409ded7398c4e11880a1
zaee07 <ihzasofyansah@gmail.com>
2025-08-27 03:17:35 +0700

    style: Merapikan Tampilan Pengumuman Siswa

--- resources/views/pengumuman.blade.php
+++ resources/views/pengumuman.blade.php
@@ -8,30 +8,43 @@
 @endphp
 
 <div class="container">
-    <h1>Pengumuman Hasil Seleksi</h1>
-    <p>Berikut adalah hasil seleksi siswa yang sudah diverifikasi.</p>
-
-    <div class="row">
-        <div class="col-md-6">
-            <h4>✅ Diterima</h4>
-            <ul>
-                @forelse($diterima as $s)
-                    <li>{{ $s->nama }} - {{ $s->nisn }}</li>
-                @empty
-                    <li>Belum ada data.</li>
-                @endforelse
-            </ul>
+    <div class="card shadow-sm">
+        <div class="card-header bg-primary text-white">
+            <h4 class="mb-0"><i class="fa fa-bullhorn"></i> Pengumuman Hasil Seleksi</h4>
         </div>
+        <div class="card-body">
+            <p class="lead">Berikut adalah daftar siswa yang dinyatakan <strong>DITERIMA</strong> dan <strong>DITOLAK</strong> dalam proses seleksi.</p>
+            <hr>
+            @if($diterima->isEmpty() && $ditolak->isEmpty())
+                        <div class="alert alert-info text-center mt-4">
+                            <h4 class="alert-heading"><i class="fa fa-clock"></i> Menunggu Hasil</h4>
+                            <p class="mb-0">Proses seleksi masih berlangsung. Silakan periksa kembali halaman ini nanti.</p>
+                        </div>
+            @else
+            <div class="row">
+                <div class="col-md-6">
+                    <h4>✅ Diterima</h4>
+                    <ul>
+                        @forelse($diterima as $s)
+                            <li>{{ $s->nama }} - {{ $s->nisn }}</li>
+                        @empty
+                            <li>Belum ada data.</li>
+                        @endforelse
+                    </ul>
+                </div>
 
-        <div class="col-md-6">
-            <h4>❌ Ditolak</h4>
-            <ul>
-                @forelse($ditolak as $s)
-                    <li>{{ $s->nama }} - {{ $s->nisn }}</li>
-                @empty
-                    <li>Belum ada data.</li>
-                @endforelse
-            </ul>
+                <div class="col-md-6">
+                    <h4>❌ Ditolak</h4>
+                    <ul>
+                        @forelse($ditolak as $s)
+                            <li>{{ $s->nama }} - {{ $s->nisn }}</li>
+                        @empty
+                            <li>Belum ada data.</li>
+                        @endforelse
+                    </ul>
+                </div>
+            </div>
+            @endif
         </div>
     </div>
 </div>
