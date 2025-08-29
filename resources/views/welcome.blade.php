a958aa1ca42b9e52f4516f3f8708ae2846fd2bac
NanChill22 <muhamadnanda022@gmail.com>
2025-08-29 11:22:12 +0700

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PPDB NanChill 2025</title>
+    <title>PPDB sekolah 2025</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
